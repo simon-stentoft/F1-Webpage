409ac55580934af12f1f457615fcce99fec48019
Simon <57576661+simon-stentoft@users.noreply.github.com>
2023-03-19 00:52:24 +0100

    Added CSS to make brand appear red when hovered

--- formula1/src/App.js
+++ formula1/src/App.js
@@ -5,17 +5,6 @@ import Home from "./components/Home";
 import RaceSchedule from './components/RaceSchedule';
 import DriverStandings from './components/DriverStandingList';
 
-/*function App() {
-  return (
-    <div>
-      <h1 className="title is-2">Formula 1</h1>
-      <div>
-        {<ResultList/>}
-      </div>
-    </div>
-  );
-}
-export default App;*/
 
 function App() {
   return (
@@ -23,7 +12,7 @@ function App() {
       <div>
         <nav className="navbar text-bg-dark">
            <div className="container">
-            <a href='/' className='navbar-brand text-bg-dark'>Formula1</a>
+            <a href='/' className='navbar-brand fw-bold'>Formula1</a>
              <Link to='/driverlist' className="nav-item nav-link active" aria-current="page">Driver list</Link>
              <Link to='/recentresultlist' className="nav-item nav-link active" aria-current="page">Most recent race results</Link>
              <Link to='/raceschedule' className="nav-item nav-link active" aria-current="page">Current season race schedule</Link>
